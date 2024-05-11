0ab2184b993a4e5c36232439e55599de498f7bc9
Hasindu Ramanayake <86105866+HasinduRamanayake@users.noreply.github.com>
2024-05-11 18:37:04 +0530

    Fixed the Like Icon Bug

--- frontend/js/backbone_app/views/Posts/post_view.js
+++ frontend/js/backbone_app/views/Posts/post_view.js
@@ -2,10 +2,10 @@ var PostDetailView = Backbone.View.extend({
    
     events: {
         'submit .comment-form': 'createComment',
-        'click .delete-comment': 'deleteComment',
-        'click .edit-comment': 'toggleEdit',
-        'click .save-comment': 'saveComment',
-        'click .cancel-comment': 'toggleEdit',
+        'click .delete-comment-button': 'deleteComment',
+        'click .edit-comment-button': 'toggleEdit',
+        'click .save-comment-button': 'saveComment',
+        'click .cancel-comment-button': 'toggleEdit',
         'click .like-post': 'likePost',
     },
     initialize: function(options) {
@@ -30,17 +30,15 @@ var PostDetailView = Backbone.View.extend({
             self.commentFormTemplate = _.template(wrapper.find('#comment-form-detail-template').html());
             
             if (self.postTemplate && self.commentTemplate && self.commentFormTemplate) {
-                // Fetch post data first
                 self.model.fetch().done(function(response) {
                     var responseData = self.model.get('data') || {};
 
-                    // Now, access likes from the responseData
                     var likes = responseData.likes || [];
 
                     console.log('likes',responseData);
                    
                     console.log('likes',likes);
-                    // Check if any of the likes belong to the current user
+                    // Checking if any of the likes belong to the current user
                     var likedByCurrentUser = likes.some(function(like) {
                         return like.user_id == self.currentUser && responseData.id == self.model.id;
                     });
@@ -49,12 +47,12 @@ var PostDetailView = Backbone.View.extend({
 
                     self.postRender(likedByCurrentUser);
     
-                    // Then fetch comments
+                    // Fetch comments
                     self.collection.fetch({
                         reset: true,
                         success: function(collection) {
                             console.log("Comments fetch success", collection);
-                            // Render comments and form after comments are fetched
+                            // Render comments and form 
                             self.commentRender();
                             self.commentFormRender();
                         },
@@ -76,7 +74,6 @@ var PostDetailView = Backbone.View.extend({
     },
 
     postRender: function(isLiked) {
-        // Instead of passing isLiked as an argument, get it from the model directly
         var modelData = this.model.toJSON();
         var data = modelData.data;
         var isLiked = this.model.get('likedByCurrentUser');
@@ -84,12 +81,12 @@ var PostDetailView = Backbone.View.extend({
         if (this.postTemplate) {
             this.$el.html(this.postTemplate(_.extend(data, { likedByCurrentUser: isLiked })));
             
-            // Update the like button text and class
+            
             var likeButton = this.$('.like-post');
-            likeButton.text(isLiked ? 'Unlike' : 'Like');
-            likeButton.toggleClass('liked', isLiked);
+            likeButton.toggleClass('liked', !isLiked);
+            likeButton.find('i').toggleClass('fas fa-heart far fa-heart');
 
-            // Ensure that events are delegated after rendering
+            // Ensuring that events are delegated after rendering
             this.delegateEvents();
         } else {
             console.error('Template has not been loaded.');
@@ -153,29 +150,29 @@ var PostDetailView = Backbone.View.extend({
         var self = this;
         var postId = this.model.id;
         
-        // Check if the post is already liked by the user
+        
         var isLiked = this.model.get('likedByCurrentUser');
         
-        // Determine the API endpoint based on whether the post is already liked or not
+        // Selecting the API endpoint based on whether the post is already liked or not
         var endpoint = isLiked ? 'remove_like' : 'add_like';
         var methodType = isLiked ? 'DELETE' : 'POST';
         
-        // Send a request to the server to like/unlike the post
+        // sending request to the server to like/unlike the post
         $.ajax({
             url: 'http://localhost/toonflix/api/likes/' + endpoint + '/' + postId,
             type: methodType,
             success: function(response) {
                 console.log('Post ' + (isLiked ? 'unliked' : 'liked') + ' successfully:', response);
                 
-                // Update the model with the new like status
+                // Updatign the model with the new like status
                 self.model.set('likedByCurrentUser', !isLiked);
                 
-                // Update the button directly, rather than re-rendering the whole post
+                // Updating the button ICon
                 var likeButton = self.$('.like-post');
-                likeButton.text(!isLiked ? 'Unlike' : 'Like'); // Switch the text based on new like status
-                likeButton.toggleClass('liked', !isLiked); // Toggle the class based on new like status
+                likeButton.toggleClass('liked', !isLiked);
+                likeButton.find('i').toggleClass('fas fa-heart far fa-heart');  
 
-                // Optionally trigger a model change event to update other parts of the view
+                // Trigger a model change event 
                 self.model.trigger('change:likedByCurrentUser');
             },
             error: function(xhr, status, error) {
@@ -186,34 +183,25 @@ var PostDetailView = Backbone.View.extend({
     
 
     toggleEdit: function(e) {
-        e.preventDefault();  
+        e.preventDefault();
         
-        
-        console.log("Toggle edit called");
-        var commentId = $(e.currentTarget).data('id');
-        var selector = '[data-id="' + commentId + '"]';
-        var isEditing = this.$(selector + ' .edit-comment-text').is(':visible');
+        var $commentContainer = $(e.currentTarget).closest('.comment-item');
     
-        console.log("Is editing:", isEditing);
-
+        var isEditing = $commentContainer.find('.edit-comment-text').is(':visible');
+    
         if (isEditing) {
-            // Hide edit controls
-            this.$(selector + ' .edit-comment-text').css('display', 'none');
-            this.$(selector + ' .comment-text').css('display', 'block');
-            this.$(selector + ' .edit-comment').css('display', 'inline');
-            this.$(selector + ' .delete-comment').css('display', 'inline');
-            this.$(selector + ' .save-comment').css('display', 'none');
-            this.$(selector + ' .cancel-comment').css('display', 'none');
+            // Switch To view mode
+            $commentContainer.find('.comment-text, .edit-comment-button, .delete-comment-button').show();
+            $commentContainer.find('.edit-comment-text, .save-comment-button, .cancel-comment-button').hide();
         } else {
-            // Show edit controls
-            this.$(selector + ' .edit-comment-text').css('display', 'block');
-            this.$(selector + ' .comment-text').css('display', 'none');
-            this.$(selector + ' .edit-comment').css('display', 'none');
-            this.$(selector + ' .delete-comment').css('display', 'none');
-            this.$(selector + ' .save-comment').css('display', 'inline');
-            this.$(selector + ' .cancel-comment').css('display', 'inline');
+            // Switch to edit Mode
+            $commentContainer.find('.comment-text, .edit-comment-button, .delete-comment-button').hide();
+            $commentContainer.find('.edit-comment-text, .save-comment-button, .cancel-comment-button').show();
         }
     },
+    
+    
+    
 
     saveComment: function(e) {
         var commentId = $(e.currentTarget).data('id');
@@ -223,7 +211,7 @@ var PostDetailView = Backbone.View.extend({
         if (comment) {
             comment.save({content: commentText}, {
                 success: (model, response) => {
-                    this.commentRender();  // Re-render to show updated comment
+                    this.commentRender();  // Rerendering the all comments
                     this.toggleEdit(e);    // Toggle back to view mode
                 },
                 error: (model, response) => {
@@ -233,21 +221,21 @@ var PostDetailView = Backbone.View.extend({
         }
     },
 
-    cancelEdit: function(e) {
-        var commentId = $(e.currentTarget).data('id');
-        this.commentRender();  // Just re-render the comments to reset edit states
+    cancelEdit: function() {
+        this.toggleEdit();
     },
 
     deleteComment: function(e) {
         e.preventDefault();
         var commentId = $(e.currentTarget).data('id');
         var comment = this.collection.get(commentId);
-
+    
         if (comment) {
+            comment.set('destroyed', true); // Setting the destroyed flag
             comment.destroy({
                 success: (model, response) => {
                     console.log('Comment deleted successfully:', response);
-                    this.commentRender();  // Re-render the comment list
+                    this.commentRender();  // Rerender the comment list
                 },
                 error: (model, response) => {
                     console.error('Failed to delete comment:', response);
@@ -255,6 +243,7 @@ var PostDetailView = Backbone.View.extend({
             });
         }
     }
+    
 
    
 });
